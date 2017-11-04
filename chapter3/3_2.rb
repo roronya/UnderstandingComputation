703fc76ae4fedb855a28a0c4a1a0fe01094b5202
roronya <yukikanai0204@gmail.com>
2017-11-04 16:35:14 +0900

    3.2.3

--- chapter3/3_2.rb
+++ chapter3/3_2.rb
@@ -67,41 +67,43 @@ class NFADesign < Struct.new(:start_states, :accept_states, :rulebook)
   end
 end
 
+if __FILE__ == $0
 # 3.2.1
-rulebook = NFARulebook.new([
-    FARule.new(1, 'a', 1), FARule.new(1, 'b', 1), FARule.new(1, 'b', 2),
-    FARule.new(2, 'a', 3), FARule.new(2, 'b', 3),
-    FARule.new(3, 'a', 4), FARule.new(3, 'b', 4)
-                           ])
-puts(rulebook.next_states(Set[1], 'b').inspect)
-puts(rulebook.next_states(Set[1, 2], 'a').inspect)
-puts(rulebook.next_states(Set[1, 3], 'b').inspect)
+  rulebook = NFARulebook.new([
+                                 FARule.new(1, 'a', 1), FARule.new(1, 'b', 1), FARule.new(1, 'b', 2),
+                                 FARule.new(2, 'a', 3), FARule.new(2, 'b', 3),
+                                 FARule.new(3, 'a', 4), FARule.new(3, 'b', 4)
+                             ])
+  puts(rulebook.next_states(Set[1], 'b').inspect)
+  puts(rulebook.next_states(Set[1, 2], 'a').inspect)
+  puts(rulebook.next_states(Set[1, 3], 'b').inspect)
 
-puts(NFA.new(Set[1], [4], rulebook).accepting?)
-puts(NFA.new(Set[1, 2, 4], [4], rulebook).accepting?)
+  puts(NFA.new(Set[1], [4], rulebook).accepting?)
+  puts(NFA.new(Set[1, 2, 4], [4], rulebook).accepting?)
 
-nfa = NFA.new(Set[1], [4], rulebook); puts(nfa.accepting?)
-nfa.read_character('b'); puts(nfa.accepting?)
-nfa.read_character('a'); puts(nfa.accepting?)
-nfa.read_character('b'); puts(nfa.accepting?)
-nfa = NFA.new(Set[1], [4], rulebook); puts(nfa.accepting?)
-nfa.read_string('bbbbb'); puts(nfa.accepting?)
+  nfa = NFA.new(Set[1], [4], rulebook); puts(nfa.accepting?)
+  nfa.read_character('b'); puts(nfa.accepting?)
+  nfa.read_character('a'); puts(nfa.accepting?)
+  nfa.read_character('b'); puts(nfa.accepting?)
+  nfa = NFA.new(Set[1], [4], rulebook); puts(nfa.accepting?)
+  nfa.read_string('bbbbb'); puts(nfa.accepting?)
 
-nfa_design = NFADesign.new(1, [4], rulebook)
-#puts(nfa_design.accepts?('bab'))
-#puts(nfa_design.accepts?('bbbbb'))
-#puts(nfa_design.accepts?('bbabb'))
+  nfa_design = NFADesign.new(Set[1], [4], rulebook)
+  puts(nfa_design.accepts?('bab'))
+  puts(nfa_design.accepts?('bbbbb'))
+  puts(nfa_design.accepts?('bbabb'))
 
 # 3.2.2
-rulebook = NFARulebook.new([
-    FARule.new(1, nil, 2), FARule.new(1, nil, 4),
-    FARule.new(2, 'a', 3), FARule.new(3, 'a', 2),
-    FARule.new(4, 'a', 5), FARule.new(5, 'a', 6),
-    FARule.new(6, 'a', 4)
-                           ])
-puts(rulebook.next_states(Set[1], nil).inspect)
-puts(rulebook.follow_free_moves(Set[1]).inspect)
-nfa = NFA.new(Set[1], [2,4], rulebook)
-nfa.read_string('aaa'); puts(nfa.accepting?)
-nfa.read_string('aaaa'); puts(nfa.accepting?)
+  rulebook = NFARulebook.new([
+                                 FARule.new(1, nil, 2), FARule.new(1, nil, 4),
+                                 FARule.new(2, 'a', 3), FARule.new(3, 'a', 2),
+                                 FARule.new(4, 'a', 5), FARule.new(5, 'a', 6),
+                                 FARule.new(6, 'a', 4)
+                             ])
+  puts(rulebook.next_states(Set[1], nil).inspect)
+  puts(rulebook.follow_free_moves(Set[1]).inspect)
+  nfa = NFA.new(Set[1], [2,4], rulebook)
+  nfa.read_string('aaa'); puts(nfa.accepting?)
+  nfa.read_string('aaaa'); puts(nfa.accepting?)
+end
 
--- chapter3/3_3.rb
+++ chapter3/3_3.rb
@@ -1,3 +1,6 @@
+require 'set'
+require './3_2'
+
 module Pattern
   def bracket(outer_precedence)
     if precedence < outer_precedence
@@ -9,6 +12,10 @@ module Pattern
 
   def inspect
     "/#{self}/"
+  end
+
+  def matches?(string)
+    to_nfa_design.accepts?(string)
   end
 end
 
@@ -22,6 +29,14 @@ class Empty
   def precedence
     3
   end
+
+  def to_nfa_design
+    start_state = Object.new
+    accept_states = start_state
+    rulebook = NFARulebook.new([])
+
+    NFADesign.new(Set[start_state], [accept_states], rulebook)
+  end
 end
 
 class Literal < Struct.new(:character)
@@ -34,6 +49,15 @@ class Literal < Struct.new(:character)
   def precedence
     3
   end
+
+  def to_nfa_design
+    start_state = Object.new
+    accept_state = Object.new
+    rule = FARule.new(start_state, character, accept_state)
+    rulebook = NFARulebook.new([rule])
+
+    NFADesign.new(Set[start_state], [accept_state], rulebook)
+  end
 end
 
 class Concatenate < Struct.new(:first, :second)
@@ -45,6 +69,23 @@ class Concatenate < Struct.new(:first, :second)
 
   def precedence
     1
+  end
+
+  def to_nfa_design
+    first_nfa_design = first.to_nfa_design
+    second_nfa_design = second.to_nfa_design
+
+    start_state = first_nfa_design.start_states
+    accept_states = second_nfa_design.accept_states
+    rules = first_nfa_design.rulebook.rules + second_nfa_design.rulebook.rules
+    extra_rules = first_nfa_design.accept_states.flat_map{ |first_state|
+      second_nfa_design.start_states.map{ |second_state|
+        FARule.new(first_state, nil , second_state)
+      }
+    }
+    rulebook = NFARulebook.new(rules + extra_rules)
+
+    NFADesign.new(start_state, accept_states, rulebook)
   end
 end
 
@@ -59,6 +100,25 @@ class Choose < Struct.new(:first, :second)
   def precedence
     0
   end
+
+  def to_nfa_design
+    first_nfa_design = first.to_nfa_design
+    second_nfa_design = second.to_nfa_design
+
+    start_state = Object.new
+    accept_states = first_nfa_design.accept_states + second_nfa_design.accept_states
+
+    rules = first_nfa_design.rulebook.rules + second_nfa_design.rulebook.rules
+    first_extra_rules = first_nfa_design.start_states.map{ |state|
+      FARule.new(start_state, nil, state)
+    }
+    second_extra_rules = second_nfa_design.start_states.map{ |state|
+      FARule.new(start_state, nil, state)
+    }
+    rulebook = NFARulebook.new(rules + first_extra_rules + second_extra_rules)
+
+    NFADesign.new(Set[start_state], accept_states, rulebook)
+  end
 end
 
 class Repeat < Struct.new(:pattern)
@@ -71,12 +131,83 @@ class Repeat < Struct.new(:pattern)
   def precedence
     2
   end
+
+  def to_nfa_design
+    pattern_nfa_design = pattern.to_nfa_design
+
+    start_state = Object.new
+    accept_state = pattern_nfa_design.accept_states + [start_state]
+    rules = pattern_nfa_design.rulebook.rules
+    extra_rules =
+        pattern_nfa_design.accept_states.flat_map { |accept_state|
+          pattern_nfa_design.start_states.map{ |start_state|
+            FARule.new(accept_state, nil, start_state)
+          }
+        } +
+        pattern_nfa_design.start_states.map{ |state|
+          FARule.new(start_state, nil, state)
+        }
+    rulebook = NFARulebook.new(rules + extra_rules)
+
+    NFADesign.new(Set[start_state], accept_state, rulebook)
+  end
 end
 
-pattern = Repeat.new(
-                  Choose.new(
-                            Concatenate.new(Literal.new('a'), Literal.new('b')),
-                            Literal.new('a')
-                  )
-)
-puts(pattern.inspect)
+if __FILE__ == $0
+  pattern = Repeat.new(
+      Choose.new(
+          Concatenate.new(Literal.new('a'), Literal.new('b')),
+          Literal.new('a')
+      )
+  )
+  puts(pattern.inspect)
+
+  nfa_design = Empty.new.to_nfa_design
+  puts(nfa_design.accepts?(''))
+  puts(nfa_design.accepts?('a'))
+  nfa_design = Literal.new('a').to_nfa_design
+  puts(nfa_design.accepts?(''))
+
+  puts(nfa_design.accepts?('b'))
+
+  puts(Empty.new.matches?('a')) #=> false
+  puts(Literal.new('a').matches?('a')) #=> true
+
+  pattern = Concatenate.new(
+      Concatenate.new(Literal.new('a'), Literal.new('b')),
+      Literal.new('c')
+  )
+  puts(pattern.matches?('a')) #=> false
+  puts(pattern.matches?('ab')) #=> false
+  puts(pattern.matches?('abc')) #=> true
+
+  pattern = Choose.new(
+                      Literal.new('a'),
+                      Literal.new('b')
+  )
+  puts(pattern.matches?('b')) #=> true
+  puts(pattern.matches?('a')) #=> true
+  puts(pattern.matches?('c')) #=> false
+  pattern = Repeat.new(Literal.new('a'))
+  puts(pattern.matches?('')) #=> true
+  puts(pattern.matches?('a')) #=> true
+  puts(pattern.matches?('aaaa')) #=> true
+  puts(pattern.matches?('b')) #=> false
+
+  pattern =
+      Repeat.new(
+                Concatenate.new(
+                               Literal.new('a'),
+                               Choose.new(Empty.new, Literal.new('b'))
+                )
+      )
+  puts(pattern.inspect)
+  puts(pattern.matches?('')) #=> true
+  puts(pattern.matches?('a')) #=> true
+  puts(pattern.matches?('ab')) #=> true
+  puts(pattern.matches?('aba')) #=> true
+  puts(pattern.matches?('abab')) #=> true
+  puts(pattern.matches?('abaab')) #=> true
+  puts(pattern.matches?('abba')) #=> false
+end
+
